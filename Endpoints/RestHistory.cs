b89155a80ce8365547c91524075659d934d953d3
Andrey Mizerov <andrey@mizerov.com>
2022-11-14 20:58:14 +0300

    GetRests

---
+++ Endpoints/RestHistory.cs
@@ -0,0 +1,18 @@
+﻿using AmassWebApi.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmassWebApi.Endpoints;
+
+public static class RestHistory
+{
+    //Extension class
+    public static void Map(this WebApplication app)
+    {
+        app.MapGet("/RestHistory/{UserID}", Get);
+    }
+    public static async Task<IResult> Get(AmassDb db, int UserID)
+    {
+        var res = await db.Database.ExecuteSqlRawAsync("report_DailyUserRests @UserID", UserID);
+        return Results.Ok(res);
+    }
+}
